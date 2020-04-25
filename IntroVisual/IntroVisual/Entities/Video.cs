ced2a2d1e5b0005ee8a1ebcf16a6ef95fad21114
Tresor Tshimanga <tshims79@gmail.com>
2020-04-25 20:56:47 +0200

    This is my first Razor App that I am learing MVC Core with. Let hope that my journey will be amusing
    I am a software tester for now but I wish from this course I will be able to build a MVC app and do the software testing myself as well.

---
+++ IntroVisual/IntroVisual/Entities/Video.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroVisual.Entities
+{
+    public class Video
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int GenreId { get; set; }
+    }
+}
